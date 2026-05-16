0ca53463e672571b52cd8210b5cd68cc9a082486
Ezra2121-et <getnety2121@gmail.com>
2026-05-16 16:52:47 +0300

    initial project scaffold for governance and audit services

---
+++ governance-service/src/test/java/com/dengene/governance_service/GovernanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dengene.governance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
