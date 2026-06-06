17155de92680a4f02105483d6c1751477bedaf91
Ezra2121-et <getnety2121@gmail.com>
2026-06-06 12:07:03 +0300

    adding the audit model and auditlog repository for audit service

---
+++ audit-service/src/main/java/com/dengene/audit_service/model/AuditLog.java
@@ -0,0 +1,20 @@
+package com.dengene.audit_service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "audit_logs")
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String eventType;
+    private Long policyId;
+    private String actor;
+    private LocalDateTime timestamp;
+}
---
+++ audit-service/src/main/java/com/dengene/audit_service/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.dengene.audit_service.repository;
+
+import com.dengene.audit_service.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+}
